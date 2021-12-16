1537e0352165e0544db02dadf76dc48b50f8460a
sup2is <dev.sup2is@gmail.com>
2021-12-16 21:28:12 +0900

    프라이탁 신제품 알람 생성기 작성

--- main.py
+++ main.py
@@ -1,57 +1,84 @@
 import datetime
-import subprocess
 import sys
 from time import sleep
+import threading
 
 import chromedriver_autoinstaller
 import redis
 import telegram
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-product_name = sys.argv[1]
-print("### product_name : " +  product_name);
-my_token = ""
-user_id = ""
-bot = telegram.Bot(token = my_token)
-chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]  #크롬드라이버 버전 확인
-redis = redis.StrictRedis(host='localhost', port=6379, db=0)
+def get_new_items(urls, keys, chose_items, idx):
+    idx %= len(chose_items)
+    itemIdx = int(chose_items[idx])
+    try:
+        driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver')
+    except:
+        chromedriver_autoinstaller.install(True)
+        driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver')
 
-subprocess.Popen(r'/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --user-data-dir="~\chrometemp"', shell=True)
+    driver.get(url=urls[itemIdx])
 
-option = Options()
-option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR,
+                                                                    "#block-freitag-content > article > section:nth-child(2) > div > div > div > div > div > div > div > div > a")))
 
-try:
-    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver', options=option)
-except:
-    chromedriver_autoinstaller.install(True)
-    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver', options=option)
+    click = driver.find_element(By.CSS_SELECTOR,
+                                '#block-freitag-content > article > section:nth-child(2) > div > div > div > div > div > div > div > div > a')
+    driver.execute_script("arguments[0].click();", click)
 
-URL = 'https://www.freitag.ch/en/f52'
+    elements = driver.find_elements(By.CSS_SELECTOR, 'section.layout picture img');
 
-driver.get(url=URL)
+    count = 0
 
-picture = WebDriverWait(driver, 5000).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#block-freitag-content > article > section:nth-child(2) > div > div > div > div > div > div > div > div > a")))
+    for item in elements:
+        image_src = item.get_attribute('src')
+        alt = item.get_attribute(':alt')
 
-click = driver.find_element(By.CSS_SELECTOR, '#block-freitag-content > article > section:nth-child(2) > div > div > div > div > div > div > div > div > a')
-driver.execute_script("arguments[0].click();", click)
+        if alt is not None and not redis.exists(keys[itemIdx] + ":" + image_src):
+            redis.set(
+                keys[itemIdx] + ":" + image_src,
+                image_src,
+                datetime.timedelta(days=30)
+            )
+            bot.sendMessage(chat_id=user_id, text=image_src)
+            count += 1
 
-elements = driver.find_elements(By.CSS_SELECTOR, 'section.layout picture img');
+        sleep(1)
 
-for item in elements:
-    image_src = item.get_attribute('src')
-    alt = item.get_attribute(':alt')
+    print("new ({}) count : {}".format(keys[itemIdx], count))
+    threading.Timer(1200, get_new_items, [urls, keys, chose_items, idx + 1]).start() # 20분간격으로 번갈아가면서 동작
 
-    if alt is not None and not redis.exists(product_name + ":" + image_src):
-        sleep(0.5)
-        redis.set(
-            product_name + ":" + image_src,
-            image_src,
-            datetime.timedelta(days=30)
-        )
-        bot.sendMessage(chat_id=user_id, text=image_src)
+if __name__ == '__main__':
+
+    my_token = "" # telegram 토큰입력
+    user_id = "" # telegram user_id 입력
+    bot = telegram.Bot(token=my_token)
+    chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]  # 크롬드라이버 버전 확인
+    redis = redis.StrictRedis(host='localhost', port=6379, db=0)
+    sys.setrecursionlimit(10001)
+
+    dict = {"miami": "https://www.freitag.ch/en/f52",
+           "dragnet": "https://www.freitag.ch/en/f12",
+           "lassie": "https://www.freitag.ch/en/f11",
+           "dexter": "https://www.freitag.ch/en/f14",
+           "leland": "https://www.freitag.ch/en/f202",
+           "bob": "https://www.freitag.ch/en/f203"}
+
+    print("\n****\nChoose two \nexample: 1 2 or 2 5 or 3 3\n****\n")
+
+    for idx, item in enumerate(dict):
+        print(str(idx) + ": " + item)
+
+    chose_items = input().split()
+
+    keys = list(dict.keys())
+    urls = list(dict.values())
+
+    print("chose item: {}, {}".format(keys[int(chose_items[0])], keys[int(chose_items[1])]))
+    print("It runs every 30 minutes.")
+    get_new_items(urls, keys, chose_items, 0)
+
 
